3b076fe3a1bcae3ad43224ca1bd4af11e6c24c28
siddharth-372 <siddharthmaha372@gmail.com>
2021-02-13 21:11:51 +0530

    13-02-2021

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,22 @@ class TodosController < ApplicationController
   def index
     render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
   end
+
+  def show
+    todo = Todo.find(params[:id])
+    render plain: todo.to_pleasant_string
+  end
+
+  def edit
+    todo = Todo.find(params[:id])
+    todo.completed = true
+    render plain: todo.to_pleasant_string
+  end
+
+  def create
+    todotext = params[:todo_text]
+    tododate = params[:due_date]
+    todo = Todo.create!(todo_text: todotext, due_date: Date.today + tododata, completed: false)
+    render plain: todo.to_pleasant_string
+  end
 end
