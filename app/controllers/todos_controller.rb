d1b0ba0899625eb36b46eb085caf5118df0e7f07
siddharth-372 <siddharthmaha372@gmail.com>
2021-02-13 15:18:15 +0530

    todo first

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    render plain: Todo.all.map { |todo| todo.to_pleasant_string }.join("\n")
   end
 end
